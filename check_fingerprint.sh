7ba108b769f7f744f825e3c8171fce6489e7878a
Enrico Perera <enrico.perera@rometlimited.com>
2022-08-15 01:50:37 -0400

    Added script to check fingerprint of certificate

--- check_fingerprint.sh
+++ check_fingerprint.sh
@@ -2,16 +2,15 @@
 
 if [[ $# -gt 0 ]]
 then
-    
-    # MYFILE_EXT= $MYFILE#*.
+        
     MYEXT=${1##*.}  
     if [[ $MYEXT = "pem" || $MYEXT = "crt" ]]
     then
-        openssl x509 -in $1 -noout -text
-    elif [[ $MYEXT = "csr" ]]
-    then
-        openssl req -text -noout -verify -in $1
+        openssl x509 -in $1 -noout -sha256 -fingerprint   
+    else
+        echo "Need pem or crt to fingerprint"
     fi
+
     # 
 else
     echo "Missing Path to certificate"
