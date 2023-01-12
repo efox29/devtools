ebb8aad81b377d9f2b2fefa51aad4d73eb9f71f1
Enrico Perera <enrico.perera@rometlimited.com>
2023-01-12 13:23:17 -0500

    Check if mouse moves more than 10 pixels
    check if the mouse moves more than 10 pixels and if so, turn off script.

--- mouse.py
+++ mouse.py
@@ -3,13 +3,23 @@
 import pyautogui
 import time
 
-
+print("Starting...")
+prev_x, prev_y = pyautogui.position()
 while True:
-        pyautogui.move(-1, 0)
+    pyautogui.move(-1, 0)
 
-        # Wait for 1 second
-        time.sleep(10)
+    # Wait for 10 second
+    time.sleep(10)
 
-        # Move the mouse cursor one pixel to the right
-        pyautogui.move(1, 0)
-        pyautogui.click()
+    # Move the mouse cursor one pixel to the right
+    pyautogui.move(1, 0)
+    pyautogui.click()
+
+    #check distance
+    x, y = pyautogui.position()
+    distance = ((x - prev_x) ** 2 + (y - prev_y) ** 2) ** 0.5
+    if distance > 10:
+        break
+    prev_x, prev_y = x, y
+
+print("Exiting...")
